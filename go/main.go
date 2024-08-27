8c0d94961eb653afe459db46a653159a73de33d5
Cade Cuddy <cadecuddy@gmail.com>
2024-08-27 15:46:37 -0500

    Change repo structure & only output on simulated trade

---
+++ go/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+
+	"coinbase-scalper/pkg/operations"
+	"coinbase-scalper/pkg/utils"
+	"sync"
+)
+
+const (
+	profitTarget   = 0.00025 // .005%
+	stopLoss       = 0.00025 // .0025%
+	initialBalance = 10000
+)
+
+var (
+	lastPrice  float64
+	position   bool
+	mu         sync.Mutex
+	balance    float64
+	btcHolding float64
+	entryPrice float64
+)
+
+func main() {
+	balance = initialBalance
+
+	err := godotenv.Load()
+	if err != nil {
+		utils.LogAndPrintError("Error loading .env file: %v", err)
+	}
+
+	for {
+		initialPosition := position
+		price, _, _, _ := operations.GetAssetDetails("BTC-USD")
+
+		mu.Lock() // not actually used rn because no concurrency but putting here for later
+
+		if !position && (lastPrice > 0 && price < lastPrice*(1-stopLoss)) {
+			operations.SimulateBuy(price, &balance, &btcHolding, &entryPrice, &position)
+		} else if position && (price >= entryPrice*(1+profitTarget) || price <= entryPrice*(1-stopLoss)) {
+			operations.SimulateSell(price, &balance, &btcHolding, &entryPrice, &position)
+		}
+
+		lastPrice = price
+		mu.Unlock() // see above mu.Lock()
+
+		if initialPosition != position {
+			fmt.Printf("Balance: $%.2f\n", balance)
+			fmt.Printf("BTC Holding: %.8f\n", btcHolding)
+			fmt.Println()
+		}
+	}
+}
